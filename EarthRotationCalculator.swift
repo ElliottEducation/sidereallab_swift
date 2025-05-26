4008808317b2df53d8b5f9529126d977bfef2b2f
Elliott Education <elliotteducation956@gmail.com>
2025-05-26 10:18:25 +1000

    Create EarthRotationCalculator.swift

---
+++ EarthRotationCalculator.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+struct EarthRotationCalculator {
+    static let a: Double = 6378137.0
+    static let eSquared: Double = 0.00669437999014
+    
+    static func effectiveRadius(latitude: Double) -> Double {
+        let phi = latitude * .pi / 180
+        let numerator = a * cos(phi)
+        let denominator = sqrt(1 - eSquared * pow(sin(phi), 2))
+        return numerator / denominator
+    }
+    
+    static func angularVelocity(deltaT: Double) -> Double {
+        return 2 * .pi / deltaT
+    }
+    
+    static func siderealSpeed(latitude: Double, deltaT: Double) -> Double {
+        let radius = effectiveRadius(latitude: latitude)
+        let omega = angularVelocity(deltaT: deltaT)
+        return omega * radius
+    }
+}
+

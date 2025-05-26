f63c79818f0962cd0c26d098723b745918891822
Elliott Education <elliotteducation956@gmail.com>
2025-05-26 10:23:07 +1000

    Create ShareSheet.swift

---
+++ ShareSheet.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct ShareSheet: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        return UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities
+        )
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+        // No update needed
+    }
+}
+
